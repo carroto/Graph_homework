75bf30becce547aa2569358e8684c08ec19e9545
carroto <Liqi6178@163.com>
2022-10-03 19:10:23 +0800

    添加项目文件。

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace homework
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ListBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ListBox_SelectionChanged_2(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+    public class graph
+    {
+        //construct the data structure
+
+        public void create()
+        {
+            //create graph by the input box
+            //or it will be NULL
+        }
+        public void algorithm1()
+        {
+            //once choiced in the box, use this to travel
+        }
+        public void travel()
+        {
+            //parameter: choice in the front box:  algorithm   travel-way
+
+            //while traveling, update the open-closed form
+            //draw the node on the front page
+        }
+    }
+}
