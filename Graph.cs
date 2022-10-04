adbefa70f7f9bed0211373046448ce0b92d0d652
ewoifuoi <hamimelons@126.com>
2022-10-04 19:57:31 +0800

    bfs建图

--- Graph.cs
+++ Graph.cs
@@ -40,7 +40,7 @@ namespace homework
         }
         public void add(int s, int t, int w)
         {
-            nodeList[s-1].Add(new Node(s - 1,t-1, w));
+            nodeList[s - 1].Add(new Node(s - 1, t - 1, w));
         }
         public List<List<Node>> getList()
         {
@@ -51,7 +51,7 @@ namespace homework
             List<List<Node>> res = new List<List<Node>>();
             que = new Queue<int>();
             int[] vis = new int[count];
-            if(nodeList == null)
+            if (nodeList == null)
             {
                 MessageBox.Show("bfs 失败：当前图为空");
                 return null;
@@ -65,16 +65,16 @@ namespace homework
             while (que.Count > 0)
             {
                 int now = que.First();
-                
-                for(int i = 0; i < nodeList[now].Count; i++)
+
+                for (int i = 0; i < nodeList[now].Count; i++)
                 {
 
                     if (vis[nodeList[now][i].t] == 0)
                     {
-                        
+
                         que.Enqueue(nodeList[now][i].t);
-                        
-                        
+
+
                         vis[nodeList[now][i].t] = vis[nodeList[now][i].s] + 1;
                         res[vis[nodeList[now][i].t]].Add(nodeList[now][i]);
                     }
@@ -83,40 +83,6 @@ namespace homework
             }
             return res;
         }
-        public List<List<Node>> getList()
-        {
-            return nodeList;
-        }
-        public List<List<Node>> bfs(int s)
-        {
-            List<List<Node>> res = new List<List<Node>>();
-            que = new Queue<int>();
-            int[] vis = new int[count];
-            if(nodeList == null)
-            {
-                MessageBox.Show("bfs 失败：当前图为空");
-                return null;
-            }
-            que.Enqueue(s);
-            vis[s] = 1;
-            
-           while(que.Count > 0)
-            {
-                int now = que.First();
-                res.Add(new List<Node>());
-                for(int i = 0; i < nodeList[now].Count; i++)
-                {
-                    res[res.Count-1].Add(nodeList[now][i]);
 
-                    if (vis[nodeList[now][i].t] == 0)
-                    {
-                        que.Enqueue(nodeList[now][i].t);
-                        vis[nodeList[now][i].t] = 1;
-                    }
-                }
-                que.Dequeue();
-            }
-            return res;
-        }
     }
 }
