412ca51634389478ad617ebb529d3ee4b1de19c0
ewoifuoi <hamimelons@126.com>
2022-10-08 17:59:18 +0800

    部分重构

--- Draw.cs
+++ Draw.cs
@@ -4,17 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using System.Windows.Shapes;
+using System.Windows;
+using System.Windows.Media;
+
 
 namespace homework
 {
-    
-
-
     /// <summary>
     /// 绘图类
     /// </summary>
     public class Draw
     {
+        /// <summary>
+        /// 圆周率
+        /// </summary>
+        public static double PI = 3.1415926535;
 
         /// <summary>
         /// 存放箭头两边终点坐标的类型
@@ -27,8 +32,6 @@ namespace homework
             public double Y2;
         }
 
-
-
         /// <summary>
         /// 为直线绘制箭头的函数，直接将目标画布的引用传入完成绘制
         /// (在循环画边的时候使用)
@@ -36,14 +39,30 @@ namespace homework
         /// <param name="playground">目标画布的引用</param>
         /// <param name="hx">箭头顶点的横坐标</param>
         /// <param name="hy">箭头顶点的纵坐标</param>
-        /// <param name="theta">箭头与直线的夹角</param>
+        /// <param name="a">箭头与直线的夹角</param>
         /// <param name="len">箭头的长度</param>
-        /// <param name="k">直线的斜率</param>
-        public static void DrawArrow(ref Canvas playground, double hx, double hy, double theta, double len, double k)
+        /// <param name="L">直线(用于计算斜率)</param>
+        public static void DrawArrow(ref Canvas playground, double hx, double hy, double a, double len, Line L)
         {
-            
+            Line l1 = new Line();
+            Line l2 = new Line();
+            testTarget(ref playground, hx, hy);
         }
 
-        
+        /// <summary>
+        /// 测试箭头顶点位置函数
+        /// </summary>
+        /// <param name="playground">目标画布的引用</param>
+        /// <param name="hx">顶点横坐标/param>
+        /// <param name="hy">顶点纵坐标</param>
+        private static void testTarget(ref Canvas playground, double hx, double hy)
+        {
+            Ellipse mark = new Ellipse();
+            mark.Height = 5; mark.Width = 5;
+            mark.Fill = Brushes.Red;
+            Canvas.SetLeft(mark, hx); Canvas.SetTop(mark, hy);
+            Canvas.SetZIndex(mark, 9);
+            playground.Children.Add(mark);
+        }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -186,11 +186,9 @@ namespace homework
                     l.X2 = graph_data[nodeslist[i][j].t].x; l.Y2 = graph_data[nodeslist[i][j].t].y;
                     Canvas.SetZIndex(l, 0);
 
-                    ///////////--------------------------------------------------------------------//////////////
+                    ///////////---------------以下为确定箭头终点-----------------------------------------------------//////////////
 
-                    Ellipse mark = new Ellipse();
-                    mark.Height = 5; mark.Width = 5;
-
+ 
                     double k = (l.Y2 - l.Y1) / (l.X2 - l.X1);
                     //- -   右半区+下顶点正确
                     //即  l.x1 < l.x2 时结果正确
@@ -210,13 +208,9 @@ namespace homework
                         xm += r * Math.Cos(Math.Atan(k));
                         ym += r * Math.Sin(Math.Atan(k));
                     }
-                    
-                    mark.Fill = Brushes.Red;
-                    Canvas.SetLeft(mark, xm); Canvas.SetTop(mark, ym);
-                    Canvas.SetZIndex(mark, 9);
-                    playground.Children.Add(mark);
-                        
+       
                     playground.Children.Add(l);
+                    Draw.DrawArrow(ref playground, xm, ym, Draw.PI / 6, 10, l);
                 }
             }
         }
