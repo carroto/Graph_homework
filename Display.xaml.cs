49c282c702670090012593c1f6914e6a609a713d
ewoifuoi <hamimelons@126.com>
2022-10-09 15:44:05 +0800

    实现整个画布在外部Canvas中平移

--- Display.xaml.cs
+++ Display.xaml.cs
@@ -21,12 +21,49 @@ namespace homework
     {
 
 
-        
+        private bool _isMouseDown = false;
+        Point _mouseDownPosition;
+        Point _mouseDownControlPosition;
 
         public Display()
         {
             InitializeComponent();
             
+        }
+
+        private void test_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var c = sender as Canvas;
+            _isMouseDown = true;
+            _mouseDownPosition = e.GetPosition(this);
+            _mouseDownControlPosition = new Point(double.IsNaN(Canvas.GetLeft(c)) ? 0 : Canvas.GetLeft(c), double.IsNaN(Canvas.GetTop(c)) ? 0 : Canvas.GetTop(c));
+            c.CaptureMouse();
+
+        }
+
+        private void test_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_isMouseDown)
+            {
+                var c = sender as Canvas;
+                var pos = e.GetPosition(this);
+                var dp = pos - _mouseDownPosition;
+                Canvas.SetLeft(c, _mouseDownControlPosition.X + dp.X);
+                Canvas.SetTop(c, _mouseDownControlPosition.Y + dp.Y);
+            }
+
+        }
+
+        private void test_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            var c = sender as Canvas;
+            _isMouseDown = false;
+            c.ReleaseMouseCapture();
+        }
+
+        private void test_Wheel(object sender, MouseWheelEventArgs e)
+        {
+            //MessageBox.Show("wheel up");
         }
     }
 }
